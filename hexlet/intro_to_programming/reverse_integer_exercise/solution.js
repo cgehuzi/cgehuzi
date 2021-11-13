baf52421eab7db70c5c2ae5d9e332a959f72d59a
Ihar Spurhiash <mail.cgehuzi@gmail.com>
2021-11-13 16:18:25 +0100

    [ solution ] - Переворот числа (Введение в программирование)

---
+++ hexlet/intro_to_programming/reverse_integer_exercise/solution.js
@@ -0,0 +1,11 @@
+const reverseInt = (num) => {
+    const num_str = String(num);
+    let result = '';
+    for (let i = 0; i < num_str.length; i += 1) {
+        const char = num_str[i];
+        result = char === '-' ? `${result}` : `${char}${result}`;
+    }
+    return num < 0 ? Number(`-${result}`) : Number(result);
+}
+
+export default reverseInt;
---
+++ hexlet/intro_to_programming/reverse_integer_exercise/test.js
@@ -0,0 +1,7 @@
+import reverseInt from './solution.js';
+
+test('Переворот числа' + '\n  # ' + 'Введение в программирование', () => {
+    expect(reverseInt(13)).toBe(31);
+    expect(reverseInt(-123)).toBe(-321);
+    expect(reverseInt(8900)).toBe(98);
+});
