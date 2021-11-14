a177320df0e20852fb31237214cb12ddbe2ed859
Ihar Spurhiash <mail.cgehuzi@gmail.com>
2021-11-15 00:10:52 +0100

    [ solution ] - Найди Fizz и Buzz

---
+++ hexlet/intro_to_programming/fizzbuzz_exercise/solution.js
@@ -0,0 +1,20 @@
+const fizzBuzz = (begin, end) => {
+    if (begin > end) {
+        return false;
+    }
+
+    for (let i = begin; i <= end; i += 1) {
+        const fizz = i % 3 === 0 ? 'Fizz' : '';
+        const buzz = i % 5 === 0 ? 'Buzz' : '';
+
+        if (fizz !== '' || buzz !== '') {
+            console.log(`${fizz}${buzz}`);
+        } else {
+            console.log(i);
+        }
+    }
+
+    return true;
+}
+
+export default fizzBuzz;
