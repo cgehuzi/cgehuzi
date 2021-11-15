0868ef4edffa51145ba6029a08c46ddec1ea22f1
Ihar Spurhiash <mail.cgehuzi@gmail.com>
2021-11-15 16:47:12 +0100

    [ solution ] - Без двух нулей

---
+++ hexlet/intro_to_programming/without_two_zeros_exercise/solution.js
@@ -0,0 +1,26 @@
+const factorial = (num) => {
+    if (num === 0) {
+        return 1;
+    }
+    return num * factorial(num - 1);
+}
+
+const countPositions = (a, b) => factorial(a + b) / (factorial(a) * factorial(b));
+
+const withoutTwoZeros = (zero, one) => {
+    if (zero - one > 1) {
+        return 0;
+    }
+
+    const iter = (acc, a, b) => {
+        if (a === 0) {
+            return acc;
+        }
+
+        return iter(acc - countPositions(a, b), a - 1, b);
+    }
+
+    return iter(countPositions(zero, one), zero - 1, one);
+}
+
+export default withoutTwoZeros;
---
+++ hexlet/intro_to_programming/without_two_zeros_exercise/test.js
@@ -0,0 +1,11 @@
+import withoutTwoZeros from './solution.js';
+
+test('Без двух нулей' + '\n  # ' + 'Введение в программирование', () => {
+    expect(withoutTwoZeros(2, 2)).toBe(3);
+    expect(withoutTwoZeros(1, 1)).toBe(2);
+    expect(withoutTwoZeros(1, 3)).toBe(4);
+    expect(withoutTwoZeros(2, 4)).toBe(10);
+    expect(withoutTwoZeros(3, 1)).toBe(0);
+    expect(withoutTwoZeros(5, 4)).toBe(1);
+    expect(withoutTwoZeros(3, 2)).toBe(1);
+});
